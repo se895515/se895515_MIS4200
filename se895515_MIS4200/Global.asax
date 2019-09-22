d363662c66b45001630618ad039f9ef9a5d79797
Ellis <se895515@ohio.edu>
2019-09-22 19:41:01 -0400

    Add project files.

---
+++ se895515_MIS4200/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="se895515_MIS4200.MvcApplication" Language="C#" %>
---
+++ se895515_MIS4200/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(se895515_MIS4200.Startup))]
+namespace se895515_MIS4200
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
